3a4edfc80be5b65b25ce15fccbf8345cfe52d092
Arkadiy <alibullterrier@gmail.com>
2023-06-02 06:56:58 +0300

    modification store

--- src/state/store.ts
+++ src/state/store.ts
@@ -14,4 +14,3 @@ export const store = legacy_createStore(rootReducer)
 export type AppRootStateType = ReturnType<typeof rootReducer>
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
-window.store = store
