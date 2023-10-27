af1bb30a7c250c0bcb9f12077693ee6bec6c55b5
Arkadiy <alibullterrier@gmail.com>
2023-10-28 00:08:28 +0300

    refactor: editableSpan to arrow syntax

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,12 +1,12 @@
-import React, {ChangeEvent, useState} from 'react';
-import {TextField} from "@mui/material";
+import React, { ChangeEvent, useState } from 'react';
+import { TextField } from "@mui/material";
 
 type EditableSpanPropsType = {
     value: string
     onChange: (newValue: string) => void
 }
 
-export function EditableSpan(props: EditableSpanPropsType) {
+export const EditableSpan = (props: EditableSpanPropsType) => {
     let [editMode, setEditMode] = useState(false);
     let [title, setTitle] = useState(props.value);
 
@@ -23,7 +23,7 @@ export function EditableSpan(props: EditableSpanPropsType) {
     }
 
     return editMode
-        ?    <TextField variant="outlined"
-                        value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode} />
+        ? <TextField variant="outlined"
+            value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode} />
         : <span onDoubleClick={activateEditMode}>{props.value}</span>
 }
